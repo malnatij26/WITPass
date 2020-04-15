9e63ef2b2afd1aec1569423d3816aeb471965297
Josh Malnati <malnatij@wit.edu>
2020-04-15 16:20:00 -0400

    Read NFC

--- UI/read.js
+++ UI/read.js
@@ -12,20 +12,45 @@ export default class Read extends Component {
     Actions.write()
   }
 
+  componentDidMount() {
+    NfcManager.start();
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
+      console.warn('tag', tag);
+      NfcManager.setAlertMessageIOS('I got your tag!');
+      NfcManager.unregisterTagEvent().catch(() => 0);
+    });
+
+    //read tag
+    try {
+      await NfcManager.registerTagEvent(eventMsg);
+      alert(eventMsg)
+    } catch (ex) {
+      console.warn('ex', ex);
+      NfcManager.unregisterTagEvent().catch(() => 0);
+    }
+  }
+
+  componentWillUnmount() {
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+    NfcManager.unregisterTagEvent().catch(() => 0);
+  }
+
+  _cancel = () => {
+    NfcManager.unregisterTagEvent().catch(() => 0);
+  }
+
+
   render() {
     return(
       <View>
-        {/* <Form type="Login"/> */}
-        <View>
-          <Row style={{justifyContent:'center'}}>
-            <Image source={require('./nfc_logo.png')} style={{width: 100, height: 100}} />
-          </Row>
-        
-          <Text>Place phone near an NFC tag, or</Text>
-          <TouchableOpacity onPress={this.write}>
-              <Text style={styles.btn}>Go to Write</Text>
-          </TouchableOpacity>
-      </View>
+        <Row style={{justifyContent:'center'}}>
+          <Image source={require('./nfc_logo.png')} style={{width: 100, height: 100}} />
+        </Row>
+      
+        <Text>Place phone near an NFC tag, or</Text>
+        <TouchableOpacity onPress={this.write}>
+            <Text style={styles.btn}>Go to Write</Text>
+        </TouchableOpacity>
       </View>
     );
   }
